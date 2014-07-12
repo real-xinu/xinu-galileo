f2e87985652647b13056ac53f1c6695eae51f10a
Rajas H Karandikar <rkarandi@purdue.edu>
2014-07-12 15:09:11 -0700

    Misc. changes to shell commands. Removed acpi. platinit in initialize

--- shell/xsh_ipaddr.c
+++ shell/xsh_ipaddr.c
@@ -11,8 +11,6 @@
  */
 shellcmd xsh_ipaddr(int nargs, char *args[]) {
 
-	int32	iface;			/* inferface number		*/
-	struct	ifentry	*ifptr;		/* ptr to interface table entry	*/
 	uint32	ipaddr;			/* an IP address in binary	*/
 	uint32	ipbcast;		/* IP broadcast addr. in binary	*/
 	uint32	ipprefix;		/* IP network prefix in binary	*/
@@ -27,7 +25,7 @@ shellcmd xsh_ipaddr(int nargs, char *args[]) {
 		printf("Description:\n");
 		printf("\tDisplays IP address information\n");
 		printf("Options:\n");
-		printf("\t-f\tforce a new DHCP request for interface 0\n");
+		printf("\t-f\tforce a new DHCP request\n");
 		printf("\t--help\tdisplay this help and exit\n");
 		return OK;
 	}
@@ -49,95 +47,81 @@ shellcmd xsh_ipaddr(int nargs, char *args[]) {
 				args[0]);
 			return 1;
 		}
-		if ( (if_tab[0].if_state != IF_UP) ||
-					(host && (ifprime != 0)) ) {
-			fprintf(stderr, "%s: -f invalid (needs iface 0)\n",
-							args[0]);
+		NetData.ipvalid = FALSE;
+		ipaddr = getlocalip();
+		if((int32)ipaddr == SYSERR) {
+			fprintf(stderr, "Cannot get IP address using DHCP\n");
 			return 1;
 		}
-		if_tab[0].if_ipvalid = FALSE;
 	}
 
-	/* Verify that at least one network interface is "up" */
+	printf("\n");
 
-	if (ifprime < 0) {
-		fprintf(stderr, "%s: no network interface is up\n",
-								args[0]);
-		return 1;
-	}
+	/* IP unicast address in dotted decimal and hex */
 
-	for (iface=0; iface<NIFACES; iface++) {
-		ifptr = &if_tab[iface];
-		if (ifptr->if_state != IF_UP) {
-		continue;
-		}
-		printf("\nFor interface %d:\n", iface);
+	ipaddr = NetData.ipucast;
+	sprintf(str, "%d.%d.%d.%d",
+		(ipaddr>>24)&0xff, (ipaddr>>16)&0xff,
+		(ipaddr>>8)&0xff,        ipaddr&0xff);
+	printf("   %-15s  %-15s  0x%08x\n",
+		"IP address:", str, ipaddr);
 
-		/* IP unicast address in dotted decimal and hex */
+	/* IP network broadcast address in dotted decimal & hex	*/
 
-		ipaddr = ifptr->if_ipucast;
-		sprintf(str, "%d.%d.%d.%d",
-			(ipaddr>>24)&0xff, (ipaddr>>16)&0xff,
-			(ipaddr>>8)&0xff,        ipaddr&0xff);
-		printf("   %-15s  %-15s  0x%08x\n",
-			"IP address:", str, ipaddr);
+	ipbcast = NetData.ipbcast;
+	sprintf(str, "%d.%d.%d.%d",
+		(ipbcast>>24)&0xff, (ipbcast>>16)&0xff,
+		(ipbcast>>8)&0xff,        ipbcast&0xff);
+	printf("   %-15s  %-15s  0x%08x\n",
+		"IP broadcast:", str, ipbcast);
 
-		/* IP network broadcast address in dotted decimal & hex	*/
+	/* IP network prefix in dotted decimal & hex */
 
-		ipbcast = ifptr->if_ipbcast;
-		sprintf(str, "%d.%d.%d.%d",
-			(ipbcast>>24)&0xff, (ipbcast>>16)&0xff,
-			(ipbcast>>8)&0xff,        ipbcast&0xff);
-		printf("   %-15s  %-15s  0x%08x\n",
-			"IP broadcast:", str, ipbcast);
+	ipprefix = NetData.ipprefix;
+	sprintf(str, "%d.%d.%d.%d",
+		(ipprefix>>24)&0xff, (ipprefix>>16)&0xff,
+		(ipprefix>>8)&0xff,        ipprefix&0xff);
+	printf("   %-15s  %-15s  0x%08x\n",
+		"IP prefix:", str, ipprefix);
 
-		/* IP network prefix in dotted decimal & hex */
+	/* IP network mask in dotted decimal & hex */
 
-		ipprefix = ifptr->if_ipprefix;
-		sprintf(str, "%d.%d.%d.%d",
-			(ipprefix>>24)&0xff, (ipprefix>>16)&0xff,
-			(ipprefix>>8)&0xff,        ipprefix&0xff);
-		printf("   %-15s  %-15s  0x%08x\n",
-			"IP prefix:", str, ipprefix);
+	ipmask = NetData.ipmask;
+	ipaddr = NetData.ipucast;
+	sprintf(str, "%d.%d.%d.%d",
+		(ipmask>>24)&0xff, (ipmask>>16)&0xff,
+		(ipmask>>8)&0xff,        ipmask&0xff);
+	printf("   %-15s  %-15s  0x%08x\n",
+		"Address mask:", str, ipmask);
 
-		/* IP network mask in dotted decimal & hex */
+	/* Default router in dotted decimal & hex */
 
-		ipmask = ifptr->if_ipmask;
-		ipaddr = ifptr->if_ipucast;
-		sprintf(str, "%d.%d.%d.%d",
-			(ipmask>>24)&0xff, (ipmask>>16)&0xff,
-			(ipmask>>8)&0xff,        ipmask&0xff);
-		printf("   %-15s  %-15s  0x%08x\n",
-			"Address mask:", str, ipmask);
-
-		/* Default router in dotted decimal & hex */
-
-		router = ifptr->if_iprouter;
-		sprintf(str, "%d.%d.%d.%d",
-			(router>>24)&0xff, (router>>16)&0xff,
-			(router>>8)&0xff,        router&0xff);
-		printf("   %-15s  %-15s  0x%08x\n",
-			"IP router:", str, router);
+	router = NetData.iprouter;
+	sprintf(str, "%d.%d.%d.%d",
+		(router>>24)&0xff, (router>>16)&0xff,
+		(router>>8)&0xff,        router&0xff);
+	printf("   %-15s  %-15s  0x%08x\n",
+		"IP router:", str, router);
 
 
-		printf("   %-15s  %02x:%02x:%02x:%02x:%02x:%02x\n",
-		"MAC unicast:",
-		0xff & ifptr->if_macucast[0],
-		0xff & ifptr->if_macucast[1],
-		0xff & ifptr->if_macucast[2],
-		0xff & ifptr->if_macucast[3],
-		0xff & ifptr->if_macucast[4],
-		0xff & ifptr->if_macucast[5]);
+	printf("   %-15s  %02x:%02x:%02x:%02x:%02x:%02x\n",
+	"MAC unicast:",
+	0xff & NetData.ethucast[0],
+	0xff & NetData.ethucast[1],
+	0xff & NetData.ethucast[2],
+	0xff & NetData.ethucast[3],
+	0xff & NetData.ethucast[4],
+	0xff & NetData.ethucast[5]);
 
-		printf("   %-15s  %02x:%02x:%02x:%02x:%02x:%02x\n",
-		"MAC broadcast:",
-		0xff & ifptr->if_macbcast[0],
-		0xff & ifptr->if_macbcast[1],
-		0xff & ifptr->if_macbcast[2],
-		0xff & ifptr->if_macbcast[3],
-		0xff & ifptr->if_macbcast[4],
-		0xff & ifptr->if_macbcast[5]);
-	}
+	printf("   %-15s  %02x:%02x:%02x:%02x:%02x:%02x\n",
+	"MAC broadcast:",
+	0xff & NetData.ethbcast[0],
+	0xff & NetData.ethbcast[1],
+	0xff & NetData.ethbcast[2],
+	0xff & NetData.ethbcast[3],
+	0xff & NetData.ethbcast[4],
+	0xff & NetData.ethbcast[5]);
+
 	printf("\n");
 	return OK;
 }
--- system/main.c
+++ system/main.c
@@ -3,63 +3,11 @@
 #include <xinu.h>
 #include <stdio.h>
 
-int32	main(void)
-
+process	main(void)
 {
-	uint32	retval;
-//	byte	mac1[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
-//	char	buf[1500];
-//	struct	netpacket pkt;
-//	int32	i;
-//	bool8	done;
+	/* Start the network */
 
-	kprintf("\n\n###########################################################\n\n");
-
-//	done = FALSE;
-//	while (!done) {
-//		printf("Enter s for sender or r for receiver: ");
-//		retval = read(CONSOLE, buf, 20);
-//		if ( retval != 2 ) {
-//			continue;
-//		}
-//		if ( (buf[0] != 'r') && (buf[0] != 's') ) {
-//			continue;
-//		} else {
-//			done = TRUE;
-//		}
-//	}
-//	e1000e_rar_set(&ethertab[0], mac1, 4);
-//	if ( buf[0] == 'r' ) {
-//		while (TRUE) {
-//			retval = read(ETHER0, buf, 1500);
-//			if (retval < 0) {
-//				continue;
-//			}
-//			if (buf[0] == mac1[0]) {
-//				kprintf("success!\n");
-//			}
-//		}
-//	} else {
-//		for (i=0; i<1500; i++) {
-//			*(i + (char *)&pkt) = 0xff&i;
-//		}
-//		memcpy(pkt.net_ethdst, mac1, 6);
-//		control(ETHER0, ETH_CTRL_GET_MAC,
-//				(int32)&pkt.net_ethsrc, 0);
-//		pkt.net_ethtype = ntohs(0x0800);
-//		while (TRUE) {
-//			retval = read(CONSOLE, buf, 100);
-//			write(ETHER0, (char *)&pkt, 1200);
-//		}
-//	}
-			
-	//kprintf("...starting the network\n");
 	netstart();
-
-	while(1) {
-		kprintf("clktime %d\n", clktime);
-		sleep(1);
-	}
 
 	kprintf("\n...creating a shell\n");
 	recvclr();
@@ -68,9 +16,9 @@ int32	main(void)
 	/* Wait for shell to exit and recreate it */
 
 	while (TRUE) {
-		retval = receive();
+		receive();
 		sleepms(200);
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
+		kprintf("\n\nMain process recreating shell\n\n");
 		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
 	}
 	return OK;
