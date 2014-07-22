79efdc619721864d2c9f2529447875d0ea26b003
Douglas Comer <comer@cs.purdue.edu>
2014-07-22 19:18:47 -0400

    changed comments in ip.h and ip.c

--- include/ip.h
+++ include/ip.h
@@ -1,4 +1,4 @@
-/* ip.h  -  constants related to IPv4 */
+/* ip.h  -  Constants related to Internet Protocol version 4 (IPv4) */
 
 #define	IP_BCAST	0xffffffff	/* IP local broadcast address	*/
 #define	IP_THIS		0xffffffff	/* "this host" src IP address	*/
@@ -7,19 +7,19 @@
 #define	IP_ICMP		1		/* ICMP protocol type for IP 	*/
 #define	IP_UDP		17		/* UDP protocol type for IP 	*/
 
-#define	IP_ASIZE	4		/* bytes in an IP address	*/
-#define	IP_HDR_LEN	20		/* bytes in an IP header	*/
+#define	IP_ASIZE	4		/* Bytes in an IP address	*/
+#define	IP_HDR_LEN	20		/* Bytes in an IP header	*/
 #define IP_VH		0x45 		/* IP version and hdr length 	*/
 
-#define	IP_OQSIZ	8		/* size of IP output queue	*/
+#define	IP_OQSIZ	8		/* Size of IP output queue	*/
 
 /* Queue of outgoing IP packets waiting for ipout process */
 
 struct	iqentry	{
-	int32	iqhead;			/* index of next packet to send	*/
-	int32	iqtail;			/* index of next free slot	*/
-	sid32	iqsem;			/* semaphore that counts pkts	*/
-	struct	netpacket *iqbuf[IP_OQSIZ];/* circular packet queue	*/
+	int32	iqhead;			/* Index of next packet to send	*/
+	int32	iqtail;			/* Index of next free slot	*/
+	sid32	iqsem;			/* Semaphore that counts pkts	*/
+	struct	netpacket *iqbuf[IP_OQSIZ];/* Circular packet queue	*/
 };
 
-extern	struct	iqentry	ipoqueue;	/* network output queue		*/
+extern	struct	iqentry	ipoqueue;	/* Network output queue		*/
