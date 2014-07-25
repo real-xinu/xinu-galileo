d6010e25f47ed0f2f1e831990416300be6ed13a1
Douglas Comer <comer@cs.purdue.edu>
2014-07-25 09:22:34 -0400

    changed comments in ttyGetc.c

--- device/tty/ttyGetc.c
+++ device/tty/ttyGetc.c
@@ -10,21 +10,23 @@ devcall	ttyGetc(
 	  struct dentry	*devptr		/* Entry in device switch table	*/
 	)
 {
-	char	ch;
+	char	ch;			/* Character to return		*/
 	struct	ttycblk	*typtr;		/* Pointer to ttytab entry	*/
 
 	typtr = &ttytab[devptr->dvminor];
 
-	/* Wait for a character in the buffer */
+	/* Wait for a character in the buffer and extract one character	*/
 
 	wait(typtr->tyisem);
-	ch = *typtr->tyihead++;		/* Extract one character	*/
+	ch = *typtr->tyihead++;
 
 	/* Wrap around to beginning of buffer, if needed */
 
 	if (typtr->tyihead >= &typtr->tyibuff[TY_IBUFLEN]) {
 		typtr->tyihead = typtr->tyibuff;
 	}
+
+	/* In cooked mode, check for the EOF character */
 
 	if ( (typtr->tyimode == TY_IMCOOKED) && (typtr->tyeof) &&
 	     (ch == typtr->tyeofch) ) {
