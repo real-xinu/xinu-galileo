09b793770524c96fe8af659e72a7c5783efb4672
Douglas Comer <comer@cs.purdue.edu>
2014-07-24 20:28:31 -0400

    reformatted clkinit.c

--- system/clkinit.c
+++ system/clkinit.c
@@ -15,28 +15,35 @@ void	clkinit(void)
 {
 	uint16	intv;		/* Clock rate in KHz			*/
 
-	/* Set interrupt vector for clock to invoke clkdisp */
+	/* Allocate a queue to hold the delta list of sleeping processes*/
+
+	sleepq = newqueue();
+
+	/* Initialize the preemption count */
+
+	preempt = QUANTUM;
+
+	/* Initialize the time since boot to zero */
+
+	clktime = 0;
+
+	/* Set interrupt vector for the clock to invoke clkdisp */
 
 	set_evec(IRQBASE, (uint32)clkdisp);
 
-	/* Clock rate is 1.190 Mhz; this is 1 ms interrupt rate */
+	/* Set the hardware clock: timer 0, 16-bit counter, rate */
+	/*   generator mode, and counter runs in binary		 */
+
+	outb(CLKCNTL, 0x34);
+
+	/* Set the clock rate to 1.190 Mhz; this is 1 ms interrupt rate */
 
 	intv = 1193;	/* Using 1193 instead of 1190 to fix clock skew	*/
 
-	sleepq = newqueue();	/* Allocate a queue to hold the delta	*/
-				/*  list of sleeping processes		*/
-	preempt = QUANTUM;	/* Initial time quantum			*/
-
-	/* Specify that seepq is initially empty */
-
-	clktime = 0;		/* Start counting seconds		*/
-
-	/* Set to: timer 0, 16-bit counter, rate generator mode,
-				and counter is binary*/
-	outb(CLKCNTL, 0x34);
-
 	/* Must write LSB first, then MSB */
+
 	outb(CLOCK0, (char) (0xff & intv) );
 	outb(CLOCK0, (char) (0xff & (intv>>8)));
+
 	return;
 }
