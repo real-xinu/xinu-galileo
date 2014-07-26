6021c20d5f34a9b2218085cbd68a2521fe518e5e
Douglas Comer <comer@cs.purdue.edu>
2014-07-25 20:21:44 -0400

    removed extern definitions for sltop and slnonempty

--- include/clock.h
+++ include/clock.h
@@ -12,6 +12,4 @@
 extern	uint32	clktime;	/* current time in secs since boot	*/
 
 extern	qid16	sleepq;		/* queue for sleeping processes		*/
-extern	int32	slnonempty;	/* nonzero if sleepq is nonempty	*/
-extern	int32	*sltop;		/* ptr to key in first item on sleepq	*/
 extern	uint32	preempt;	/* preemption counter			*/
