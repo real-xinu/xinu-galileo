0959ddd77f41b8e5ab282806e54f3b0f77ad9037
Douglas Comer <comer@cs.purdue.edu>
2014-07-27 14:36:24 -0400

    changed comments in shell.h

--- include/shell.h
+++ include/shell.h
@@ -2,12 +2,12 @@
 
 /* Size constants */
 
-#define SHELL_BUFLEN	TY_IBUFLEN+1	/* length of input buffer	*/
-#define SHELL_MAXTOK	32		/* maximum tokens per line	*/
-#define SHELL_CMDSTK	8192		/* size of stack for process	*/
+#define SHELL_BUFLEN	TY_IBUFLEN+1	/* Length of input buffer	*/
+#define SHELL_MAXTOK	32		/* Maximum tokens per line	*/
+#define SHELL_CMDSTK	8192		/* Size of stack for process	*/
 					/*    that executes command	*/
-#define	SHELL_ARGLEN	(SHELL_BUFLEN+SHELL_MAXTOK) /* argument area	*/
-#define SHELL_CMDPRIO	20		/* process priority for command	*/
+#define	SHELL_ARGLEN	(SHELL_BUFLEN+SHELL_MAXTOK) /* Argument area	*/
+#define SHELL_CMDPRIO	20		/* Process priority for command	*/
 
 /* Message constants */
 
@@ -26,33 +26,33 @@
 
 /* Messages shell displays for user */
 
-#define SHELL_PROMPT	"xsh $ "		/* prompt		*/
+#define SHELL_PROMPT	"xsh $ "		/* Prompt		*/
 #define SHELL_STRTMSG	"Welcome to Xinu!\n"	/* Welcome message	*/
-#define SHELL_EXITMSG	"Shell closed\n"	/* shell exit message	*/
-#define SHELL_SYNERRMSG	"Syntax error\n"	/* syntax error message	*/
+#define SHELL_EXITMSG	"Shell closed\n"	/* Shell exit message	*/
+#define SHELL_SYNERRMSG	"Syntax error\n"	/* Syntax error message	*/
 #define SHELL_CREATMSG	"Cannot create process\n"/* command error	*/
-#define SHELL_INERRMSG	"Cannot open file %s for input\n" /* input err	*/
-#define SHELL_OUTERRMSG	"Cannot open file %s for output\n"/* output err	*/
+#define SHELL_INERRMSG	"Cannot open file %s for input\n" /* Input err	*/
+#define SHELL_OUTERRMSG	"Cannot open file %s for output\n"/* Output err	*/
 #define SHELL_BGERRMSG	"Cannot redirect I/O or background a builtin\n"
-						/* builtin cmd err	*/
+						/* Builtin cmd err	*/
 /* Constants used for lexical analysis */
 
 #define	SH_NEWLINE	'\n'		/* New line character		*/
 #define	SH_EOF		'\04'		/* Control-D is EOF		*/
-#define	SH_AMPER	'&'		/* ampersand character		*/
-#define	SH_BLANK	' '		/* blank character		*/
-#define	SH_TAB		'\t'		/* tab character		*/
-#define	SH_SQUOTE	'\''		/* single quote character	*/
-#define	SH_DQUOTE	'"'		/* double quote character	*/
-#define	SH_LESS		'<'		/* less-than character	*/
-#define	SH_GREATER	'>'		/* greater-than character	*/
+#define	SH_AMPER	'&'		/* Ampersand character		*/
+#define	SH_BLANK	' '		/* Blank character		*/
+#define	SH_TAB		'\t'		/* Tab character		*/
+#define	SH_SQUOTE	'\''		/* Single quote character	*/
+#define	SH_DQUOTE	'"'		/* Double quote character	*/
+#define	SH_LESS		'<'		/* Less-than character	*/
+#define	SH_GREATER	'>'		/* Greater-than character	*/
 
 /* Token types */
 
-#define	SH_TOK_AMPER	0		/* ampersand token		*/
-#define	SH_TOK_LESS	1		/* less-than token		*/
-#define	SH_TOK_GREATER	2		/* greater-than token		*/
-#define	SH_TOK_OTHER	3		/* token other than those	*/
+#define	SH_TOK_AMPER	0		/* Ampersand token		*/
+#define	SH_TOK_LESS	1		/* Less-than token		*/
+#define	SH_TOK_GREATER	2		/* Greater-than token		*/
+#define	SH_TOK_OTHER	3		/* Token other than those	*/
 					/*   listed above (e.g., an	*/
 					/*   alphanumeric string)	*/
 
@@ -64,10 +64,10 @@
 
 /* Structure of an entry in the table of shell commands */
 
-struct	cmdent	{			/* entry in command table	*/
-	char	*cname;			/* name of command		*/
-	bool8	cbuiltin;		/* is this a builtin command?	*/
-	int32	(*cfunc)(int32,char*[]);/* function for command		*/
+struct	cmdent	{			/* Entry in command table	*/
+	char	*cname;			/* Name of command		*/
+	bool8	cbuiltin;		/* Is this a builtin command?	*/
+	int32	(*cfunc)(int32,char*[]);/* Function for command		*/
 };
 
 extern	uint32	ncmd;
