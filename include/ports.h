a982c5c540bf94436376137ed787e14c0693d845
Douglas Comer <comer@cs.purdue.edu>
2014-08-06 07:49:42 -0400

    changed comments in ports.h

--- include/ports.h
+++ include/ports.h
@@ -1,29 +1,29 @@
 /* ports.h - isbadport */
 
-#define	NPORTS		30		/* maximum number of ports	*/
-#define	PT_MSGS		100		/* total messages in system	*/
-#define	PT_FREE		1		/* port is free			*/
-#define	PT_LIMBO	2		/* port is being deleted/reset	*/
-#define	PT_ALLOC	3		/* port is allocated		*/
+#define	NPORTS		30		/* Maximum number of ports	*/
+#define	PT_MSGS		100		/* Total messages in system	*/
+#define	PT_FREE		1		/* Port is free			*/
+#define	PT_LIMBO	2		/* Port is being deleted/reset	*/
+#define	PT_ALLOC	3		/* Port is allocated		*/
 
-struct	ptnode	{			/* node on list of messages 	*/
-	uint32	ptmsg;			/* a one-word message		*/
-	struct	ptnode	*ptnext;	/* ptr to next node on list	*/
+struct	ptnode	{			/* Node on list of messages 	*/
+	uint32	ptmsg;			/* A one-word message		*/
+	struct	ptnode	*ptnext;	/* Pointer to next node on list	*/
 };
 
-struct	ptentry	{			/* entry in the port table	*/
-	sid32	ptssem;			/* sender semaphore		*/
-	sid32	ptrsem;			/* receiver semaphore		*/
-	uint16	ptstate;		/* port state (FREE/LIMBO/ALLOC)*/
-	uint16	ptmaxcnt;		/* max messages to be queued	*/
-	int32	ptseq;			/* sequence changed at creation	*/
-	struct	ptnode	*pthead;	/* list of message pointers	*/
-	struct	ptnode	*pttail;	/* tail of message list		*/
+struct	ptentry	{			/* Entry in the port table	*/
+	sid32	ptssem;			/* Sender semaphore		*/
+	sid32	ptrsem;			/* Receiver semaphore		*/
+	uint16	ptstate;		/* Port state (FREE/LIMBO/ALLOC)*/
+	uint16	ptmaxcnt;		/* Max messages to be queued	*/
+	int32	ptseq;			/* Sequence changed at creation	*/
+	struct	ptnode	*pthead;	/* List of message pointers	*/
+	struct	ptnode	*pttail;	/* Tail of message list		*/
 };
 
-extern	struct	ptnode	*ptfree;	/* list of free nodes		*/
-extern	struct	ptentry	porttab[];	/* port table			*/
-extern	int32	ptnextid;		/* next port ID to try when	*/
+extern	struct	ptnode	*ptfree;	/* List of free nodes		*/
+extern	struct	ptentry	porttab[];	/* Port table			*/
+extern	int32	ptnextid;		/* Next port ID to try when	*/
 					/*   looking for a free slot	*/
 
 #define	isbadport(portid)	( (portid)<0 || (portid)>=NPORTS )
