ca19044b6f7771747753a48a42d8c891cbea7792
Jim Lembke <lembkej@purdue.edu>
2014-08-10 10:31:30 -0400

    add xboot_initialize.c

---
+++ system/xboot_initialize.c
@@ -0,0 +1,166 @@
+/* initialize.c - nulluser, sysinit, sizmem */
+
+/* XBOOT - Handle system initialization and become the null process */
+
+#include <xinu.h>
+#include <string.h>
+
+extern	void	start(void);	/* Start of Xinu code			*/
+extern	void	*_end;		/* End of Xinu code			*/
+
+/* Function prototypes */
+
+extern	void main(void);	/* Main is the first process created	*/
+extern	void xdone(void);	/* System "shutdown" procedure		*/
+static	void sysinit(); 	/* Internal system initialization	*/
+extern	void meminit(void);	/* Initializes the free memory list	*/
+
+/* Declarations of major kernel variables */
+
+struct	procent	proctab[NPROC];	/* Process table			*/
+struct	sentry	semtab[NSEM];	/* Semaphore table			*/
+struct	memblk	memlist;	/* List of free memory blocks		*/
+
+/* Active system status */
+
+int	prcount;		/* Total number of live processes	*/
+pid32	currpid;		/* ID of currently executing process	*/
+
+/*------------------------------------------------------------------------
+ * nulluser - initialize the system and become the null process
+ *
+ * Note: execution begins here after the C run-time environment has been
+ * established.  Interrupts are initially DISABLED, and must eventually
+ * be enabled explicitly.  The code turns itself into the null process
+ * after initialization.  Because it must always remain ready to execute,
+ * the null process cannot execute code that might cause it to be
+ * suspended, wait for a semaphore, put to sleep, or exit.  In
+ * particular, the code must not perform I/O except for polled versions
+ * such as kprintf.
+ *------------------------------------------------------------------------
+ */
+ 
+void	nulluser()
+{	
+	/* Initialize the system */
+		
+	sysinit();
+
+	kprintf("\n\r%s\n\n\r", VERSION);
+
+	/* Enable interrupts */
+
+	enable();
+
+	/* Create a process to execute function main() */
+
+	resume (
+	   create((void *)main, INITSTK, INITPRIO, "Main process", 0,
+           NULL));
+
+	/* Become the Null process (i.e., guarantee that the CPU has	*/
+	/*  something to run when no other process is ready to execute)	*/
+
+	while (TRUE) {
+		;		/* Do nothing */
+	}
+
+}
+
+/*------------------------------------------------------------------------
+ *
+ * sysinit - intialize all Xinu data structures and devices
+ *
+ *------------------------------------------------------------------------
+ */
+static	void	sysinit()
+{
+	int32	i;
+	struct	procent	*prptr;		/* Ptr to process table entry	*/
+	struct	sentry	*semptr;	/* Prr to semaphore table entry	*/
+
+	/* Platform Specific Initialization */
+
+	platinit();
+
+	/* Initialize the interrupt vectors */
+
+	initevec();
+	
+	/* Initialize free memory list */
+	
+	meminit();
+
+	/* Initialize system variables */
+
+	/* Count the Null process as the first process in the system */
+
+	prcount = 1;
+
+	/* Scheduling is not currently blocked */
+
+	Defer.ndefers = 0;
+
+	/* Initialize process table entries free */
+
+	for (i = 0; i < NPROC; i++) {
+		prptr = &proctab[i];
+		prptr->prstate = PR_FREE;
+		prptr->prname[0] = NULLCH;
+		prptr->prstkbase = NULL;
+		prptr->prprio = 0;
+	}
+
+	/* Initialize the Null process entry */	
+
+	prptr = &proctab[NULLPROC];
+	prptr->prstate = PR_CURR;
+	prptr->prprio = 0;
+	strncpy(prptr->prname, "prnull", 7);
+	prptr->prstkbase = getstk(NULLSTK);
+	prptr->prstklen = NULLSTK;
+	prptr->prstkptr = 0;
+	currpid = NULLPROC;
+	
+	/* Initialize semaphores */
+
+	for (i = 0; i < NSEM; i++) {
+		semptr = &semtab[i];
+		semptr->sstate = S_FREE;
+		semptr->scount = 0;
+		semptr->squeue = newqueue();
+	}
+
+	/* Initialize buffer pools */
+
+	bufinit();
+
+	/* Create a ready list for processes */
+
+	readylist = newqueue();
+
+	/* Initialize the real time clock */
+
+	clkinit();
+
+	for (i = 0; i < NDEVS; i++) {
+		init(i);
+	}
+	return;
+}
+
+#define	NBPG		4096		/* Number of bytes per page	*/
+
+int32	stop(char *s)
+{
+	kprintf("%s\n", s);
+	kprintf("looping... press reset\n");
+	while(1)
+		/* Empty */;
+}
+
+int32	delay(int n)
+{
+	DELAY(n);
+	return OK;
+}
