c5e28db73635b596741beed50246987c70a51ef5
Tom Stonecypher <tom.stonecypher@intel.com>
2014-09-19 17:05:16 +0000

    Move console_init into platinit.c

--- system/kprintf.c
+++ system/kprintf.c
@@ -3,40 +3,11 @@
 #include <xinu.h>
 #include <stdarg.h>
 
+
 /*------------------------------------------------------------------------
  * kputc  -  use polled I/O to write a character to the console
  *------------------------------------------------------------------------
  */
-
-#define QUARK_CONS_PORT		1		/* Index of console UART */
-#define QUARK_CONS_BAR_INDEX	0		/* Index of console's MMIO
-						   base address register */
-
-/**
- *  console_init()
- *
- * Initialize the serial console.  The serial console is on the
- * second memory-mapped 16550 UART device.
- */
-int console_init(void)
-{
-	int	status;
-	int	pciDev;
-
-	pciDev = find_pci_device(INTEL_QUARK_UART_PCI_DID,
-				 INTEL_QUARK_UART_PCI_VID,
-				 QUARK_CONS_PORT);
-	if (pciDev < 0) {
-		/* Error finding console device */
-		return	pciDev;
-	}
-	/* Store the console device CSR base address into the console device's
-	   device table entry. */
-	status = pci_get_dev_mmio_base_addr(pciDev, QUARK_CONS_BAR_INDEX,
-					   &devtab[CONSOLE].dvcsr);
-	return status;
-}
-
 syscall kputc(byte c)	/* Character to write	*/
 {
 	struct	dentry	*devptr;
@@ -44,6 +15,11 @@ syscall kputc(byte c)	/* Character to write	*/
 
 	devptr = (struct dentry *) &devtab[CONSOLE];
 	regptr = (struct uart_csreg *)devptr->dvcsr;
+
+	/* Fail if no console device was found */
+	if (regptr == NULL) {
+		return SYSERR;
+	}
 
 	/* Repeatedly poll the device until it becomes nonbusy */
 	while ((regptr->lsr & UART_LSR_THRE) == 0) {
@@ -78,8 +54,13 @@ syscall kgetc(void)
 	devptr = (struct dentry *) &devtab[CONSOLE];
 	regptr = (struct uart_csreg *)devptr->dvcsr;
 
-	irmask = regptr->ier;       /* Save UART interrupt state.   */
-	regptr->ier = 0;            /* Disable UART interrupts.     */
+	/* Fail if no console device was found */
+	if (regptr == NULL) {
+		return SYSERR;
+	}
+
+	irmask = regptr->ier;		/* Save UART interrupt state.   */
+	regptr->ier = 0;		/* Disable UART interrupts.     */
 
 	while (0 == (regptr->lsr & UART_LSR_DR)) {
 		; /* Do Nothing */
@@ -87,7 +68,7 @@ syscall kgetc(void)
 
 	/* Read character from Receive Holding Register */
 	c = regptr->rbr;
-	regptr->ier = irmask;       /* Restore UART interrupts.     */
+	regptr->ier = irmask;		/* Restore UART interrupts.     */
 	return c;
 }
 
--- system/platinit.c
+++ system/platinit.c
@@ -1,6 +1,33 @@
 /* platinit.c - platinit */
 #include <xinu.h>
 
+#define QUARK_CONS_PORT		1		/* Index of console UART */
+#define QUARK_CONS_BAR_INDEX	0		/* Index of console's MMIO
+						   base address register */
+/*------------------------------------------------------------------
+ * console_init - initialize the serial console.  The serial console
+ * is on the second memory-mapped 16550 UART device.
+ *------------------------------------------------------------------
+ */
+int console_init(void)
+{
+	int	status;
+	int	pciDev;
+
+	pciDev = find_pci_device(INTEL_QUARK_UART_PCI_DID,
+				 INTEL_QUARK_UART_PCI_VID,
+				 QUARK_CONS_PORT);
+	if (pciDev < 0) {
+		/* Error finding console device */
+		return	pciDev;
+	}
+	/* Store the console device CSR base address into the console device's
+	   device table entry. */
+	status = pci_get_dev_mmio_base_addr(pciDev, QUARK_CONS_BAR_INDEX,
+					   &devtab[CONSOLE].dvcsr);
+	return status;
+}
+
 /*------------------------------------------------------------------
  * platinit - platform specific initialization for Galileo
  *------------------------------------------------------------------
@@ -20,7 +47,6 @@ void	platinit()
 	remove_irm_protections();
 
 	/* Intel Quark Irq Routing */
-
 	quark_irq_routing();
 
 	/* Find the Ethernet device */
