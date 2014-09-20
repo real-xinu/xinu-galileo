2205e06be4fc80454b7d5d8ae7cbaa900cd00809
Douglas Comer <comer@cs.purdue.edu>
2014-09-20 05:55:29 -0400

    removed blanks at ends of lines in pci.c

--- system/pci.c
+++ system/pci.c
@@ -4,7 +4,7 @@
 
 /*
  * Base and length of memory-mapped PCI configuration space.
- * 
+ *
  * These values are correct for Galileo and many other X86 platforms,
  * but may need to be changed for some platforms.  In the general case, BIOS
  * sets up MMConfig space and passes the base and length values to the OS via
@@ -126,7 +126,7 @@ int find_pci_device(int32 deviceID, int32 vendorID, int32 index)
 				/* Evaluate sub-functions only if the device
 				   is multi-function */
 				if (func == 0) {
-					multifunction = 
+					multifunction =
 						devfuncHdr->headerType &
 						PCI_HDR_TYPE_MULTIFUNCTION;
 					if (!multifunction) {
