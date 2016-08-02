17d0e9d3deca398ea38b0369d3ff1278485581f1
Jim Lembke <lembkej@purdue.edu>
2016-08-02 06:13:21 -0400

    reset console output

--- system/initialize.c
+++ system/initialize.c
@@ -26,6 +26,10 @@ struct	memblk	memlist;	/* List of free memory blocks		*/
 int	prcount;		/* Total number of live processes	*/
 pid32	currpid;		/* ID of currently executing process	*/
 
+/* Control sequence to reset the console colors and cusor positiion	*/
+
+#define CONSOLE_RESET " \033[0m\033[2J\033[;H"
+
 /*------------------------------------------------------------------------
  * nulluser - initialize the system and become the null process
  *
@@ -109,8 +113,10 @@ static	void	sysinit()
 
 	platinit();
 	
-	
-	kprintf("\033[39;49m\n\r%s\n\n\r", VERSION);
+	/* Reset the console */
+
+	kprintf(CONSOLE_RESET);	
+	kprintf("\n\r%s\n\n\r", VERSION);
 
 	/* Initialize the interrupt vectors */
 
