b3db137f0517218b9fbb4832705bde23ca283eaf
Rajas H. Karandikar <rkarandi@purdue.edu>
2016-08-30 14:37:45 -0400

    Fixed comments in quark_irq.h, quark_irq.c: PIRQA-D mapped to IRQ 3-6, PIRQE-H mapped to 9-12

--- system/quark_irq.c
+++ system/quark_irq.c
@@ -17,11 +17,11 @@ int32	quark_irq_routing(void)
 		return SYSERR;
 	}
 
-	/* Route PIRQ A-D to 8259 IRQ 2-5 respectively */
+	/* Route PIRQ A-D to 8259 IRQ 3-6 respectively */
 
 	pci_write_config_dword(pciinfo, PABCDRC_OFFSET, PABCDRC_VALUE);
 
-	/* Route PIRQ E-H to 8259 IRQ 6-9 respectively */
+	/* Route PIRQ E-H to 8259 IRQ 9-12 respectively */
 
 	pci_write_config_dword(pciinfo, PEFGHRC_OFFSET, PEFGHRC_VALUE);
 
