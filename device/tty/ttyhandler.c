2e7ea1df48b3a8864b0557790bfb1d825106c8d2
Douglas Comer <comer@cs.purdue.edu>
2018-10-13 01:32:41 -0400

    fixed comment in ttyhandler.c

--- device/tty/ttyhandler.c
+++ device/tty/ttyhandler.c
@@ -55,7 +55,7 @@ void ttyhandler(
 
 		resched_cntl(DEFER_START);
 
-		/* While chars avail. in UART buffer, call ttyinter_in	*/
+		/* While chars avail. in UART buffer, call ttyhandle_in	*/
 
 		while ( (csrptr->lsr & UART_LSR_DR) != 0) {
 			ttyhandle_in(typtr, csrptr);
