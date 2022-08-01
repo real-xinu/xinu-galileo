b0ea5d66e23edb954aa78aa5a88a969edf64e67a
Douglas Comer <comer@cs.purdue.edu>
2022-08-01 16:35:10 -0400

    Updated NTP time server address to pool.ntp.org

--- include/date.h
+++ include/date.h
@@ -61,7 +61,7 @@ extern	struct	dateinfo Date;		/* Global date information	*/
 #define	TIMELPORT	53678		/* local UDP port for time	*/
 
 #ifndef	TIMESERVER
-#define	TIMESERVER	"128.10.19.24"	/* IP address of NTP time server*/
+#define	TIMESERVER	"pool.ntp.org"	/* IP address of NTP time server*/
 #endif
 
 #define	TIMETIMEOUT	2000		/* timeout for time server (ms)	*/
