eb33be11b229651f42ef20f353b105b5459b26a8
Douglas Comer <comer@cs.purdue.edu>
2024-03-27 17:31:05 -0400

    Added missing files

---
+++ device/rds/rdsars.c
@@ -0,0 +1,35 @@
+/* rdsars.c - rdsars */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  rdsars - atomically resume a high-priority server process and suspend
+ *		the current process
+ *------------------------------------------------------------------------
+ */
+syscall	rdsars(
+	  pid32		pid		/* ID of theprocess to resume	*/
+	)
+{
+	intmask	mask;			/* Saved interrupt mask		*/
+	struct	procent *prptr;		/* Ptr to process's table entry	*/
+
+	mask = disable();
+	if (isbadpid(pid)) {
+		restore(mask);
+		return SYSERR;
+	}
+	/* Set current process state to suspended */
+	proctab[currpid].prstate = PR_SUSP;
+
+	/* If target process is suspended, resume it */
+
+	prptr = &proctab[pid];
+	if (prptr->prstate == PR_SUSP) {
+		ready(pid);
+	} else {
+		resched();
+	}
+	restore(mask);
+	return OK;
+}
